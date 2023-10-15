ff33c032344890a22ffee7af5bed48dde9798605
victor11s <60446086+victorponce518755@users.noreply.github.com>
2023-10-14 18:16:54 -0600

    ajustes

--- models/ModelUser.py
+++ models/ModelUser.py
@@ -23,13 +23,13 @@ class ModelUser:
         
     def get_userNoPassword(self,user_id):
         cursor = self.mysql.connection.cursor()
-        cursor.execute('SELECT id, name,is_admin, created_at,updated_at FROM identity WHERE id = %s', (user_id,))
+        cursor.execute('SELECT id, name,username ,is_admin, created_at,updated_at FROM identity WHERE id = %s', (user_id,))
         user_data = cursor.fetchone()
         print(user_data)
         cursor.close()
 
         if user_data:
-            user = UserSinPassword(user_data[0], user_data[1], user_data[2], user_data[3], user_data[4])
+            user = UserSinPassword(user_data[0], user_data[1], user_data[2], user_data[3], user_data[4], user_data[5])
             return user
         else:
             return None
