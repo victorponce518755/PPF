4e2a1f4d3d4cc5c9b0907c8e736d52502fe88f37
victor11s <60446086+victorponce518755@users.noreply.github.com>
2023-11-27 20:12:53 -0600

    ajustes

--- controllers/BoletoController.py
+++ controllers/BoletoController.py
@@ -7,10 +7,7 @@ boleto_bp = Blueprint('boleto_bp', __name__)
 @boleto_bp.route('/boleto', methods=['POST'])
 def create_boleto():
 
-    user_id = session['user_id']
     boleto_data = request.get_json()
-    boleto_data['idUsuario'] = user_id
-
     boleto_service = BoletoServices(mysql)
     boleto_service.create_boleto(boleto_data)
     return jsonify({'message': 'Boleto creado exitosamente'})
